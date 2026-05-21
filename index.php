84439a6b87f258b8dfb5f44d86092b8835cfd2fe
Davi-Freitass <davi_f_freitas@estudante.sesisenai.org.br>
2026-05-21 11:52:17 -0300

    index criado

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head
+    meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login com o Banco</title>
+</head>
+<body>
+    <h2>
+        Login com PHP
+    </h2>
+
+    <form method="post">
+    <label for="usuario">Usuario</label>
+
+    <br><br>
+
+    <label for="senha">Senha</label>
+    <input type="password" name="senha">
+
+    </form>
+
+</body>
+</html> 
